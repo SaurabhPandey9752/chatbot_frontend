0bce64fe2bb94b220e96206fa9fb545ff538f527
SaurabhPandey9752 <saurabhpandey@lightningtechnologies.net>
2025-02-09 00:40:04 +0530

    first commit

---
+++ src/routes/dashboardPage/DashboardPage.jsx
@@ -0,0 +1,68 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import "./dashboardPage.css";
+
+const DashboardPage = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const mutation = useMutation({
+    mutationFn: (text) => {
+      return fetch(`${import.meta.env.VITE_API_URL}/api/chats`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text }),
+      }).then((res) => res.json());
+    },
+    onSuccess: (id) => {
+      queryClient.invalidateQueries({ queryKey: ["userChats"] });
+      navigate(`/dashboard/chats/${id}`);
+    },
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const text = e.target.text.value;
+    if (!text) return;
+    mutation.mutate(text);
+  };
+
+  return (
+    <div className="dashboardPage">
+      <div className="texts">
+        <div className="logo">
+          <img src="/logo.png" alt="Logo" />
+          <h1>Future</h1>
+        </div>
+        <div className="options">
+          <div className="option">
+            <img src="/chat.png" alt="Chat Icon" />
+            <span>Create a New Chat</span>
+          </div>
+          <div className="option">
+            <img src="/image.png" alt="Image Icon" />
+            <span>Analyze Images</span>
+          </div>
+          <div className="option">
+            <img src="/code.png" alt="Code Icon" />
+            <span>Help me with my Code</span>
+          </div>
+        </div>
+      </div>
+      {/* Mobile View - Display Arrow */}
+      <div className="formContainer sm:hidden">
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="text" placeholder="Ask me anything..." />
+          <button>
+            <img src="/arrow.png" alt="Submit" />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
